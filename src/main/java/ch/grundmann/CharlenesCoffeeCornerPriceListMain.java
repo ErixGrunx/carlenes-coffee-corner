37df6bc82b8340a7ffe6c6e814ee0099b2fd5c00
grundmann <erichgrundmann@gmx.de>
2022-12-11 21:25:27 +0100

    user input via console

--- src/main/java/ch/grundmann/CharlenesCoffeeCornerPriceListMain.java
+++ src/main/java/ch/grundmann/CharlenesCoffeeCornerPriceListMain.java
@@ -3,30 +3,65 @@ package ch.grundmann;
 import ch.grundmann.creator.PriceListCreator;
 import ch.grundmann.product.*;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
-
-import static java.util.logging.Level.INFO;
+import java.util.Scanner;
 
 public class CharlenesCoffeeCornerPriceListMain {
 
-    private static final Logger logger = Logger.getLogger(CharlenesCoffeeCornerPriceListMain.class.getName());
-
     public static void main(String[] args) {
-        logger.log(INFO, "Generate printable price list.");
-
-        // Create your own shopping list here:
-        List<CoffeeCornerProduct> productList = List.of(
-                new Coffee(),
-                new Coffee(CoffeeSizePrice.SMALL, CoffeeExtraPrice.SPECIAL_ROAST_COFFEE),
-                new OrangeJuice(),
-                new BaconRoll()
-        );
+        var in = new Scanner(System.in);
+        System.out.println("Welcome to Charlene's coffee corner");
+        List<CoffeeCornerProduct> productList = new ArrayList<>();
+        var stopOrder = false;
+        while (!stopOrder) {
+            System.out.println("Press 1 for coffee, 2 for orange juice or 3 for bacon roll: ");
+            var product = in.nextLine();
+            if ("1".equals(product)) {
+                System.out.println("Press 1 for large, 2 for middle or 3 for small: ");
+                var coffeeSize = mapToCoffeeSizePrice(in.nextLine());
+                System.out.println("Press 1 for extra milk, 2 for foamed milk, 3 for special roast coffee or 4 for no extras: ");
+                var coffeeExtra = mapToCoffeeExtraPrice(in.nextLine());
+                productList.add(new Coffee(coffeeSize, coffeeExtra));
+            } else if ("2".equals(product)) {
+                productList.add(new OrangeJuice());
+            } else if ("3".equals(product)) {
+                productList.add(new OrangeJuice());
+            }
+            System.out.println("Press y to continue, press any key to finish order: ");
+            stopOrder = !"y".equals(in.nextLine());
+        }
 
         var priceList = new PriceListCreator().create(productList);
-        // call this method instead to use the free beverage bonus:
-        // var priceList = new PriceListCreator().createWithFreeBeverage(productList);
+        System.out.println(priceList);
+    }
 
-        logger.log(INFO, priceList);
+
+    private static CoffeeSizePrice mapToCoffeeSizePrice(String coffeeSizeEntry) {
+        CoffeeSizePrice coffeeSize;
+        if ("1".equals(coffeeSizeEntry)) {
+            coffeeSize = CoffeeSizePrice.LARGE;
+        } else if ("2".equals(coffeeSizeEntry)) {
+            coffeeSize = CoffeeSizePrice.LARGE;
+        } else if ("3".equals(coffeeSizeEntry)) {
+            coffeeSize = CoffeeSizePrice.LARGE;
+        } else {
+            coffeeSize = CoffeeSizePrice.MEDIUM;
+        } return coffeeSize;
+    }
+
+
+    private static CoffeeExtraPrice mapToCoffeeExtraPrice(String coffeeExtraEntry) {
+        CoffeeExtraPrice coffeeExtra;
+        if ("1".equals(coffeeExtraEntry)) {
+            coffeeExtra = CoffeeExtraPrice.EXTRA_MILK;
+        } else if ("2".equals(coffeeExtraEntry)) {
+            coffeeExtra = CoffeeExtraPrice.FOAMED_MILK;
+        } else if ("3".equals(coffeeExtraEntry)) {
+            coffeeExtra = CoffeeExtraPrice.SPECIAL_ROAST_COFFEE;
+        } else {
+            coffeeExtra = CoffeeExtraPrice.NONE;
+        }
+        return coffeeExtra;
     }
 }
